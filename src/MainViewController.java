b0fcbd816eebcae3298c9db7a3888f20d5d97df6
MisakaNA <sunyumeng420@gmail.com>
2021-02-16 19:42:02 -0700

    Bug fixed

--- src/MainViewController.java
+++ src/MainViewController.java
@@ -1,18 +1,19 @@
 import javafx.collections.FXCollections;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
 import javafx.scene.text.*;
+
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 
 public class MainViewController {
 
-    private HashMap<String, RatingInfo> ratingInfoHolder = new HashMap<>();
+    private final HashMap<String, RatingInfo> ratingInfoHolder = new HashMap<>();
+
     @FXML
     private ChoiceBox<String> semesterChoices = new ChoiceBox<>();
     @FXML
